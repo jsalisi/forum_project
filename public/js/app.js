60b9f77c476134141ad2cd1ae88b8163eb517abc
jsalisi <jsalisi@gmail.com>
2018-03-14 16:43:21 -0700

    some database functionality now working

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,7 @@
-// TODO: nodejs fuctionality needs to be changed
-// nodejs cannot do client side fuctions such as document as seen below
+// testing functionality
 
-//var database = require('./google-sheets-functions.js');
-
-var single_post = document.getElementById("test_paragraph");
 var button = document.getElementById("register");
 
 button.addEventListener("click", function() {
   alert("WORKS");
 });
-
-// database.loadPosts().then((post) => {
-//   console.log(post[0].post);
-//   single_post.innerHTML = post[0].post;
-// }).catch((error) => {
-//   console.log(error);
-//   console.log("Didn't work");
-// });
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const request = require('request');
+const database = require('./public/js/google-sheets-functions.js');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/test', (request, response) => {
+  database.loadPosts().then((post) => {
+    response.send(post);
+  }).catch((error) => {
+    console.log(error);
+  });
+});
+
 app.listen(8080, () => {
   console.log('Server is up on the port 8080');
-})
+});
