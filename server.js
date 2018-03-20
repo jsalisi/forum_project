a9ba0ad14500a7f10201c2ae5b6c5c6d14b457dc
jsalisi <jsalisi@gmail.com>
2018-03-19 17:52:27 -0700

    added handlebars for webpages and basic post functionality

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const request = require('request');
+const hbs = require('hbs');
+
 const database = require('./public/js/google-sheets-functions.js');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/test', (request, response) => {
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/home', (request, response) => {
   database.loadPosts().then((post) => {
-    response.send(post);
+    response.render('index.hbs', {monhun_post: post[0].post});
   }).catch((error) => {
     console.log(error);
   });
