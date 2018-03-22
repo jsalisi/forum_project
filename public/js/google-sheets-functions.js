f945b6a88b64f7da33b65c4bd54db6b124039dd0
jsalisi <jsalisi@gmail.com>
2018-03-22 15:21:33 -0700

    Added parameter comments

--- public/js/google-sheets-functions.js
+++ public/js/google-sheets-functions.js
@@ -44,6 +44,9 @@ var loadPosts = () => {
 // TODO: Add posting feature
 
 // Appends a user object to the spreadsheet
+// @param {string} user
+// @param {string} pass
+// @param {string} account_type
 var addNewUser = (user, pass, account_type) => {
   return new Promise((resolve, reject) => {
     doc.useServiceAccountAuth(creds, function(err) {
