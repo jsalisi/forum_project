006aac6e9e4f7188c022ab52cba3fd9fc5273013
boweichn <boweicheng@hotmail.com>
2018-03-23 00:04:13 -0700

    new pages created /monhun and /community

--- server.js
+++ server.js
@@ -11,21 +11,66 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+
+
 // Set views folder to be available for use
 // Set the view engine to use .hbs files for the template format
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-// Currently loading default template post to Monster Hunter announcements
-// Passes data from the database through the render parameters
+// registering Partials
+hbs.registerPartials(__dirname + '/views/partials/homePartials');
+hbs.registerPartials(__dirname + '/views/partials/monhunPartials');
+hbs.registerPartials(__dirname + '/views/partials/communityPartials');
+
+
+//*********************************Rendering*******************************//
+
+// rendering home page
 app.get('/home', (request, response) => {
   database.loadPosts().then((post) => {
-    response.render('index.hbs', {monhun_post: post[0].post});
+    response.render('index.hbs', {
+        monhun_post: post[0].post,
+        title: 'Title',
+        stats: '',
+        recent: 'Last Post'
+    });
   }).catch((error) => {
-    console.log(error);
+    response.send(error);
   });
 });
 
+// rendering monhun topic list page
+app.get('/monhun', (request, response) => {
+  database.loadPosts().then((post) => {
+    response.render('monhun.hbs', {
+        monhun_post: post[0].post,
+        title: 'Title/Thread Starter',
+        stats: 'Replies/Views',
+        recent: 'Last Post By'
+    });
+  }).catch((error) => {
+    response.send(error);
+  });
+});
+
+// rendering community topic list page
+app.get('/community', (request, response) => {
+  database.loadPosts().then((post) => {
+    response.render('community.hbs', {
+        monhun_post: post[0].post,
+        title: 'Title/Thread Starter',
+        stats: 'Replies/Views',
+        recent: 'Last Post By'
+    });
+  }).catch((error) => {
+    response.send(error);
+  });
+});
+
+
+
+//****************************Server***************************************//
 app.listen(8080, () => {
   console.log('Server is up on the port 8080');
 });
