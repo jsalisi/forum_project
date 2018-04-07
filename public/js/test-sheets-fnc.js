f7d2bb5e124ca597651c293a1e100cde04270d73
jsalisi <jsalisi@gmail.com>
2018-04-07 15:24:28 -0700

    Changed post styling

--- public/js/test-sheets-fnc.js
+++ public/js/test-sheets-fnc.js
@@ -21,7 +21,7 @@ var database = require('./google-sheets-functions.js')
 var test_post = "Hi, my name is Stephen!"
 
 /* Testing add post functionality */
-// database.addNewPost('stephen', 'new post', test_post, 5).then((result) => {
+// database.addNewPost('stephen', 'saturday', test_post, 5).then((result) => {
 //   console.log(result);
 // }).catch((error) => {
 //   console.log(error);
@@ -37,7 +37,7 @@ var test_post = "Hi, my name is Stephen!"
 /* Testing new thread functionality */
 // database.addNewThread('justin', 'How do?', test_post, '69:69').then((results) => {
 //   database.addNewPost(results.user, '22:19 apr 6, 2018', results.thread_post, 3).then((result) => {
-//     console.log(result);
+    console.log(result);
 //   }).catch((error) => {
 //     console.log(error);
 //   });
